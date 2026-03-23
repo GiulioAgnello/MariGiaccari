5b2f6cb10caee2662e7e7cefad7e9d97f51afce4
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-03-23 21:26:11 +0100

    EXPERIENCE

--- src/pages/Esperienza.jsx
+++ src/pages/Esperienza.jsx
@@ -1,7 +1,69 @@
 export default function Esperienza() {
   return (
-    <div className="esperienza-page">
-      <h1 className="text-center mt-5">Esperienza</h1>
-    </div>
+    <>
+      <section class="hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-6">
+              <div className="hero-img d-flex align-items-center justify-content-center justify-content-lg-end">
+                <img
+                  src="public\fotoHero\WEDDING-Virgjnjia-e-Simone_MARI-GIACCARI-FOTOGRAFA_-weddingplanner-weddingday-destinationwedding-weddingphotographer-unionecivile-destinationweddingphotographer-MARI-GIACCARI-FOTOGRAFA-134-1133x17.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="col-12 col-lg-6">
+              <div class="hero-content">
+                <h1>
+                  Racconto matrimoni.<em>Senza pose, senza filtri.</em>
+                </h1>
+                <p>
+                  Adotto una prospettiva unica che va oltre il convenzionale.
+                  Come fotografa di reportage di matrimonio, il mio scopo è
+                  catturare l’autenticità, la gioia e tutte le sfumature che
+                  caratterizzano il tuo giorno speciale. Il mio obiettivo cerca
+                  i momenti autentici, le risate spontanee e l’amore genuino e
+                  senza filtri che si manifesta in modo naturale. Ricerco una
+                  narrazione visiva che rifletta la vera essenza del giorno
+                  delle tue nozze, con uno stile raffinato, curato e moderno.
+                </p>
+                <div className="d-flex justify-content-center">
+                  <a href="#" class="text-decoration-none btn btn-dark ">
+                    Scopri il mio approccio
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section class="experience">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-6">
+              <div class="experience-content">
+                <h2>Esperienza</h2>
+                <p>
+                  {" "}
+                  Con oltre 10 anni di esperienza, ho avuto il privilegio di
+                  raccontare più di 200 matrimoni in Puglia e oltre. Ogni storia
+                  è unica, e il mio obiettivo è catturare l'essenza autentica di
+                  ogni coppia, creando ricordi che dureranno per sempre.
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-lg-6">
+              <div class="experience-img d-flex align-items-center justify-content-center justify-content-lg-start">
+                <img
+                  src="public\fotoHero\WEDDING-Virgjnjia-e-Simone_MARI-GIACCARI-FOTOGRAFA_-weddingplanner-weddingday-destinationwedding-weddingphotographer-unionecivile-destinationweddingphotographer-MARI-GIACCARI-FOTOGRAFA-134-1133x17.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
